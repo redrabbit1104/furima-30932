238f4682cda867ff2915082a89a6bae541c1e62c
SangJin Jung <redcaprabbit1104@gmail.com>
2020-12-02 23:15:30 +0900

    ユーザー管理機能のテストコードの修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -19,19 +19,6 @@ RSpec.describe User, type: :model do
       @user.valid?
       expect(@user.errors.full_messages).to include("Email can't be blank")
     end
-    it 'emailに@が含まれてないと登録できない' do
-      @user.email = '233'
-      @user.email.include?('@')
-      @user.valid?
-      expect(@user.errors.full_messages).to include('Email should include @')
-    end
-
-    it 'emailが重複すると登録できない' do
-      @existed_user = User.new(nickname: 'muramu', email: '111@111', password: '000000aa', password_confirmation: '000000aa', last_name: '村田', first_name: '莉子', last_name_kana: 'ムラタ', first_name_kana: 'リコ', birthday: '1933-03-03')
-      @user.email = '111@111'
-      @user.valid?
-      expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
-    end
 
     it 'passwordが空では登録できない' do
       # user = User.new(nickname: "muramu", email: "kkk@gmail.com", password: "", password_confirmation: "000000aa", last_name: "村田", first_name: "莉子", last_name_kana: "ムラタ", first_name_kana: "リコ", birthday: "1933-03-03")
@@ -46,11 +33,18 @@ RSpec.describe User, type: :model do
       @user.valid?
       expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
     end
-    it 'passwordは半角英数字混合でなければ登録できない' do
+    it 'passwordは数字のみでは登録できない' do
       @user.password = '111111'
       @user.valid?
       expect(@user.errors.full_messages).to include('Password is invalid')
     end
+
+    it 'passwordは英字のみでは登録できない' do
+      @user.password = 'aaaaaa'
+      @user.valid?
+      expect(@user.errors.full_messages).to include('Password is invalid')
+    end
+
     it 'passwordとpassword(confirmation)が一致しないと登録できない' do
       @user.password = '1111aa'
       @user.password_confirmation = '1111aaa'
@@ -86,6 +80,16 @@ RSpec.describe User, type: :model do
       @user.birthday = ''
       @user.valid?
       expect(@user.errors.full_messages).to include("Birthday can't be blank")
+    end
+
+    context '新規登録がうまくいかないとき' do      #〜の場合
+      it '重複したemailが存在する場合登録できない' do   #〜のはず
+        @user.save            #ユーザー登録しテーブルに保存
+        another_user = FactoryBot.build(:user)    #他のユーザーを生成
+        another_user.email = @user.email      #他のユーザーのemailが先に保存されている@userのemailと一緒
+        another_user.valid?       #他のユーザーのログインは有効なのか
+        expect(another_user.errors.full_messages).to include("Email has already been taken")
+      end
     end
   end
 end
