aa3925afb555a3db032e012221d1576fceb19205
SangJin Jung <redcaprabbit1104@gmail.com>
2020-12-03 23:13:38 +0900

    itemsのmigratationを再指定
    userのforiegnkeyを指定し忘れたため

--- db/migrate/20201128135044_create_items.rb
+++ db/migrate/20201128135044_create_items.rb
@@ -4,6 +4,7 @@ class CreateItems < ActiveRecord::Migration[6.0]
       t.text :name, null: false
       t.text :info, null: false
       t.integer :price, null: false
+      t.references :user, foreign_key: true
       t.timestamps
     end
   end
