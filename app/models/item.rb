30700cbe6c8aee7fc045d68e342ac5834deebc88
SangJin Jung <redcaprabbit1104@gmail.com>
2020-12-05 00:39:13 +0900

    ActiveHashのassociationなど指定

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,5 +3,16 @@ class Item < ApplicationRecord
   has_one :order
   has_one_attached :image
 
-   validates :name, :info, :image, :price,presence: true
+  validates :name, :info, :image, :price,presence: true
+
+  #ActiveHashにより生成されたカラムのassociations及びvalidation
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :shippingfee
+  belongs_to :shipplace
+  belongs_to :dateship
+
+  validates :category_id, :status_id, :shippingfee_id, :shipplace_id, :dateship_id, numericality: { other_than: 1 }
 end
