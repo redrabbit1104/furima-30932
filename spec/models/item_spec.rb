e159516223b48d35dacf324d775488941128b659
SangJin Jung <redcaprabbit1104@gmail.com>
2020-12-06 16:11:57 +0900

    テストコードの修正２

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -6,6 +6,12 @@ RSpec.describe Item, type: :model do
   end
 
   describe 'itemの保存' do
+    context 'itemが保存できる場合' do
+      it '画像、商品名、商品の詳細、カテゴリー、商品状態、発送料の負担、発送元の地域、発送までの日数、販売価格、' do
+        expect(@item).to be_valid
+      end
+    end
+
     context 'itemが保存できない場合' do
       it '画像がないと保存できない' do
         @item.image = nil
@@ -19,13 +25,19 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Name can't be blank")
       end
 
+      it '商品の詳細がないと保存できない' do
+        @item.info = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Info can't be blank")
+      end
+
       it '1以外を選ばないとカテゴリーは保存できない' do
         @item.category_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Category must be other than 1")
       end
 
-      it '1以外を選ばないと商品は保存できない' do
+      it '1以外を選ばないと商品状態は保存できない' do
         @item.status_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include('Status must be other than 1')
@@ -66,6 +78,19 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include("Price must be less than 10000000")
       end
+
+      it '全角数字では販売価格は保存できない' do
+        @item.price =１
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
+      end
+
+      it '半角文字列では販売価格は保存できない' do
+        @item.price = 'adfd'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
+      end
+
     end
   end
 end
