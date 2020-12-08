f5d78b42db623c62b3ac6a7c900b1470e53294ba
SangJin Jung <redcaprabbit1104@gmail.com>
2020-12-08 20:38:13 +0900

    コントローラー及びビューファイルの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:show]
+  before_action :set_item, only: [:show, :edit, :update]
 
   def index
     @items = Item.order("created_at DESC")
@@ -10,15 +10,26 @@ class ItemsController < ApplicationController
     @item = Item.new
   end
 
-  def show
-  end
-
   def create
     @item = Item.create(item_params)
     if @item.save
       redirect_to root_path
     else
       render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to root_path
+    else
+      render :edit
     end
   end
 
